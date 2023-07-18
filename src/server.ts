1fca267ea44872dd709c0ef6d630761bbfe4e90e
udaysarkarud <udayssarkarud@gmail.com>
2023-07-18 19:43:41 +0600

    basic route testing

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
